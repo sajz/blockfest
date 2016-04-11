e7cfd522d45894d1d8fade643acd80f0179bebc5
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2016-04-11 17:19:11 +0200

    structure

---
+++ client/body.js
@@ -0,0 +1,21 @@
+Template.body.helpers({
+	home:		function(home) {
+		if (!Session.get("tab"))
+			Session.set("tab", home);
+	},
+	tab:		function () {
+		return (Session.get("tab"));
+	},
+	session:	function (key) {
+		return (Session.get(key));
+	}
+});
+
+Template.body.events({
+	"click #accueil-tab":		function () {Session.set("tab", "accueil");		},
+	"click #program-tab":		function () {Session.set("tab", "program");		},
+	"click #participate-tab":	function () {Session.set("tab", "participate");	},
+	"click #rules-tab":			function () {Session.set("tab", "rules");		},
+	"click #team-tab":			function () {Session.set("tab", "team");		},
+	"click #info-tab":			function () {Session.set("tab", "info");		}
+});
