614678415c4a9d4d76b34d88e65305b8d5f91eb6
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2016-04-23 17:25:53 +0200

    dynamic flyer title

--- client/home/flyer.js
+++ client/home/flyer.js
@@ -2,11 +2,6 @@ Template.flyer.helpers({
 	ipfsImage: function () {
 		return ("https://upload.wikimedia.org/wikipedia/en/1/18/Ipfs-logo-1024-ice-text.png");
 	},
-	titleImage: function () {
-		var titles = 3;
-		var rand = Math.floor(Math.random() * (titles)) + 1;
-		return ("titles/" + rand + ".png");
-	},
 	coverImage: function () {
 		var dir = "QmSmjERBMSesWAMPXkzvt8DRYLNWJfHW9WZS9ZVkozgRhL";
 		var request = "https://gateway.ipfs.io/api/v0/object/get?arg=" + dir;
@@ -46,8 +41,105 @@ Template.flyer.events({
 });
 
 Template.flyer.onRendered(function () {
-	$("#fullpage").fullpage({
+	$("#flyer-container").fullpage({
 		autoScrolling: false,
 		fitToSection: false
 	});
+	var couples = [
+	{
+		title: {
+			"background-color": "#E8C3DD"
+		},
+		suptitle: {
+			"font-family": "unique",
+			"font-weight": "300",
+			"font-size": "120px",
+			"text-transform": "uppercase",
+			"color": "black"
+		},
+		subtitle: {
+			"font-size": "40px",
+			"color": "#85707E"
+		}
+	},
+	{
+		title: {
+			"background-color": "#E33E2B"
+		},
+		suptitle: {
+			"font-family": "bluebirdRegular",
+			"font-weight": "300",
+			"font-size": "120px",
+			"color": "#F3E4C8"
+		},
+		subtitle: {
+			"font-size": "40px",
+			"color": "#9E2F15"
+		}
+	},
+	{
+		title: {
+			"background-color": "#188ACC"
+		},
+		suptitle: {
+			"padding-top": "40px",
+			"padding-bottom": "20px",
+			"font-family": "retroscape",
+			"font-weight": "300",
+			"font-size": "70px",
+			"color": "#FDF600",
+			"text-transform": "uppercase"
+		},
+		subtitle: {
+			"font-size": "40px",
+			"color": "white"
+		}
+	}
+	];
+	var rand = Math.floor(Math.random() * (couples.length));
+	$("#flyer-title").css(couples[rand].title);
+	$("#flyer-suptitle").css(couples[rand].suptitle);
+	$("#flyer-subtitle").css(couples[rand].subtitle);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
