7fe1316eb9c30d7b733619bd2007550be20764f9
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2016-04-23 14:39:49 +0200

    cover image src in helper rather than onRendered callback

--- client/home/flyer.js
+++ client/home/flyer.js
@@ -6,6 +6,21 @@ Template.flyer.helpers({
 		var titles = 3;
 		var rand = Math.floor(Math.random() * (titles)) + 1;
 		return ("titles/" + rand + ".png");
+	},
+	coverImage: function () {
+		var dir = "QmSmjERBMSesWAMPXkzvt8DRYLNWJfHW9WZS9ZVkozgRhL";
+		var request = "https://gateway.ipfs.io/api/v0/object/get?arg=" + dir;
+		Meteor.http.get(request, function (err, res) {
+			if (!err)
+			{
+				var links = JSON.parse(res.content)["Links"];
+				var rand = Math.floor(Math.random() * (links.length));
+				var src = "http://ipfs.io/ipfs/" + links[rand]["Hash"];
+				$("#cover").attr("src", src);
+			}
+			else
+				console.log(err);
+		});
 	}
 });
 
@@ -18,20 +33,4 @@ Template.flyer.events({
 		event.preventDefault();
 		Session.set("upload-flyer", false);
 	}
-});
-
-Template.flyer.onRendered(function () {
-	var dir = "QmSmjERBMSesWAMPXkzvt8DRYLNWJfHW9WZS9ZVkozgRhL";
-	var request = "https://gateway.ipfs.io/api/v0/object/get?arg=" + dir;
-	Meteor.http.get(request, function (err, res) {
-		if (!err)
-		{
-			var links = JSON.parse(res.content)["Links"];
-			var rand = Math.floor(Math.random() * (links.length));
-			var src = "http://ipfs.io/ipfs/" + links[rand]["Hash"];
-			$("#cover").attr("src", src);
-		}
-		else
-			console.log(err);
-	});
 });
