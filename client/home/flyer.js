e33b743ba3bbab46ebcdf1218f2d1ee6c247d3eb
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2016-04-25 19:49:39 +0200

    flyer design

--- client/home/flyer.js
+++ client/home/flyer.js
@@ -60,6 +60,9 @@ Template.flyer.onRendered(function () {
 	//$("#flyer-info").css("background-color", fgcolor);
 	$("h2").css("color", bgcolor);
 	$("#stay-tuned-streamer").css("background-color", bgcolor);
+	$("#cover").css({
+		"border-color": bgcolor
+	});
 });
 
 function rgb2hex(rgb) {
