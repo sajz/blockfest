a3ef3b69ceecc2cd34018e30fb473b17546690e6
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2016-04-29 21:31:21 +0200

    Fixes #16: intervenants

--- client/home/home.js
+++ client/home/home.js
@@ -4,8 +4,25 @@ Template.accueil.helpers({
 		var diff = blockfest - Date.now();
 		var days = Math.floor(diff / (1000 * 60 * 60 * 24));
 		return (days);
+	},
+	teamMembersList: function () {
+		var rep = "intervenants/";
+		var list = [
+		{
+			name: "Milad Doueihi",
+			employment: "Historien des religions",
+			workingPlace: "Université de Paris-Sorbonne",
+			link: "https://fr.wikipedia.org/wiki/Milad_Doueihi",
+			filename: rep + "milad.jpg"
+		},
+		{
+			name: "Hervé Moal",
+			employment: "Directeur du Développement et de l'innovation",
+			workingPlace: "ARP-Astrance",
+			link: "#",
+			filename: rep + "herve.jpg"
+		}
+		];
+		return (list);
 	}
-});
-
-Template.accueil.onRendered(function () {
 });
