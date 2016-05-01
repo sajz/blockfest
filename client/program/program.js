25c4dcc31d3edd360e0136e8f5673c5846821250
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2016-05-01 13:58:52 +0200

    #24 picto color

--- client/program/program.js
+++ client/program/program.js
@@ -16,4 +16,24 @@ Template.program.helpers({
 Template.program.rendered = function () {
 	$("body").css("background-color", "white");
 	$("#program-header table").css("background-color", Session.get("bgcolor"));
+	jQuery('img.svg').each(function ()
+	{
+		var $img = jQuery(this);
+		var imgID = $img.attr('id');
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+
+		jQuery.get(imgURL, function(data)
+		{
+			var $svg = jQuery(data).find('svg');
+			if(typeof imgID !== 'undefined')
+				$svg = $svg.attr('id', imgID);
+			if(typeof imgClass !== 'undefined')
+				$svg = $svg.attr('class', imgClass+' replaced-svg');
+			$svg = $svg.removeAttr('xmlns:a');
+			$img.replaceWith($svg);
+			$svg.attr("fill", "white");
+		}, 'xml');
+	});
+
 }
