242eb1a9a90fcf2e1edd5691fccc6ff696322368
hanniju <hannequin.j@gmail.com>
2016-05-02 15:00:04 +0200

    new pictures + js fix + arrow fix

--- client/home/flyer.js
+++ client/home/flyer.js
@@ -4,7 +4,7 @@ Template.flyer.helpers({
 	},
 
 	coverImage: function () {
-		var rand = Math.floor(Math.random() * 6) + 1;
+		var rand = Math.floor(Math.random() * 24) + 1;
 		return ("570/" + rand + ".jpg");
 	}
 
