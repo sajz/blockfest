375f71e0b4f3e7282deecebb0a793923026832e6
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2016-05-16 18:48:53 +0200

    moved principes on home page

--- client/program/program.js
+++ client/program/program.js
@@ -27,26 +27,6 @@ Template.program.rendered = function () {
 		even++;
 	});
 
-	jQuery('img.svg').each(function ()
-	{
-		var $img = jQuery(this);
-		var imgID = $img.attr('id');
-		var imgClass = $img.attr('class');
-		var imgURL = $img.attr('src');
-
-		jQuery.get(imgURL, function(data)
-		{
-			var $svg = jQuery(data).find('svg');
-			if(typeof imgID !== 'undefined')
-				$svg = $svg.attr('id', imgID);
-			if(typeof imgClass !== 'undefined')
-				$svg = $svg.attr('class', imgClass+' replaced-svg');
-			$svg = $svg.removeAttr('xmlns:a');
-			$img.replaceWith($svg);
-			$svg.attr("fill", "white");
-		}, 'xml');
-	});
-
 }
 
 function getDays() {
