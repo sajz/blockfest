e2f2c0409805d4ba204fd8eb81412971f410137b
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2016-05-21 03:05:34 +0200

    countdown

--- client/body.js
+++ client/body.js
@@ -3,7 +3,7 @@ Template.registerHelper("session", function (key) {
 });
 
 Template.registerHelper("countDown", function () {
-	var blockfest = new Date(2016, 5, 7);
+	var blockfest = new Date(2016, 5, 10);
 	var diff = blockfest - Date.now();
 	var days = Math.floor(diff / (1000 * 60 * 60 * 24));
 	return (days);
